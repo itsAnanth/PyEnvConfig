b077fcbba5557993fad7f19aea9838d4ded7a574
itsAnanth <ananthsankar2003@gmail.com>
2025-12-26 15:59:52 +0530

    feat: get python versions script

---
+++ src/scripts/get_versions.py
@@ -0,0 +1,22 @@
+import requests
+
+URL = "https://www.python.org/api/v2/downloads/release/"
+
+def get_python_versions():
+    response = requests.get(URL, timeout=10)
+    response.raise_for_status()
+    data = response.json()
+
+    versions = [
+        release for release in data if not release['pre_release'] and "Python install manager" not in release['name']
+    ]
+
+    sorted_versions = sorted(
+        versions,
+        key=lambda v: tuple(map(int, v['name'].replace("Python ", "").split(".")))
+    )
+
+    return sorted_versions
+
+if __name__ == "__main__":
+    print(get_python_versions())
