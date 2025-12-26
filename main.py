69bbd8ac603efd1e8e7419ba23ddf2cf25142f67
itsAnanth <ananthsankar2003@gmail.com>
2025-12-26 16:49:21 +0530

    feat: include logger name for readability

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.commands.install import install_command
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='[%(asctime)s] [%(levelname)s] - %(message)s',
+    format='[%(asctime)s] [%(levelname)s] [%(name)s] - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger("pvm")
