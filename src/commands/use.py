c3d6eeba5c9928a37959cc9c2c843e36a71348d4
itsAnanth <ananthsankar2003@gmail.com>
2025-12-26 19:14:24 +0530

    feat: use a specific python version

---
+++ src/commands/use.py
@@ -0,0 +1,66 @@
+import os
+import logging
+from argparse import _SubParsersAction, ArgumentParser
+
+from src.scripts.store import Store
+from src.scripts.arch import detect_arch, is_windows
+
+logger = logging.getLogger("pvm.install")
+
+def handle_use(args):
+    index, version = Store.get_version(args.version)
+
+    if not version:
+        logger.error(f"Version {args.version} is not installed. Use pvm install {args.version} to install it.")
+        return
+    
+    shimsdir = os.path.join(Store.get_pvm_root(), "shims")
+
+    logger.debug(version)
+
+    
+    python_exe = os.path.join(version['dir'], 'python.exe')
+    
+    # Create python.cmd shim
+    python_shim = os.path.join(shimsdir, 'python.cmd')
+    with open(python_shim, 'w') as f:
+        f.write(f'@echo off\n')
+        f.write(f'"{python_exe}" %*\n')
+    
+
+    
+    # Create pip.cmd shim
+    pip_shim = os.path.join(shimsdir, 'pip.cmd')
+    with open(pip_shim, 'w') as f:
+        f.write(f'@echo off\n')
+        f.write(f'"{python_exe}" -m pip %*\n')
+    
+    logger.info(f"Using Python {args.version}")
+
+
+    version['using'] = True
+    Store.set_version(version)
+
+
+
+    
+    
+
+
+def use_command(sub_parser: _SubParsersAction[ArgumentParser]):
+
+    parser = sub_parser.add_parser(
+        'use',
+        help='Use a specific version of Python which is installed.'
+    )
+
+    parser.add_argument(
+        'version',
+        help='Python version to use',
+    )
+
+
+
+    parser.set_defaults(func=handle_use)
+
+    
