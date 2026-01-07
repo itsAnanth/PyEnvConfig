01849c6ea90fc7b88df0ed02d72a25f33b57359d
itsAnanth <ananthsankar2003@gmail.com>
2026-01-08 00:22:46 +0530

    chore: comment clarification

--- powershell/build.ps1
+++ powershell/build.ps1
@@ -1,6 +1,6 @@
 Write-Host "Building python-version-manager executable..."
 
-# Set production mode (INFO logging)
+# Set production mode (INFO level logging), 1 for development (DEBUG level logging)
 $env:PVM_DEV = "0"
 
 uv run -m PyInstaller --onefile --clean --exclude-module tests --name pvm main.py
